919e1d487049a8d8023210d5bbc7a541b30dd8c6
Hung Tu <hung.tu@techupcorp.com>
2020-11-27 11:26:48 +0700

    [coponent] - ListBuyMore and BuyMoreItem: Done!

---
+++ src/pages/ProductDetail.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="wrapper">
+    <list-buy-more />
+  </div>
+</template>
+
+<script>
+import BuyMore from "../components/BuyMore/ListBuyMore";
+export default {
+  components: {
+    "list-buy-more": BuyMore
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  width: 1200px;
+  margin: auto;
+  margin-top: 140px;
+  margin-bottom: 15px;
+}
+</style>
