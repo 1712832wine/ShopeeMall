3366068bcf20224401d795fc6d0ca33248a7f7fa
NGUYỄN HỬU TRÍ <tri.nguyen@techupcorp.com>
2020-11-30 11:09:03 +0700

    [component] - Showproduct: [Fix Transport Component]

---
+++ src/components/ShowProduct/ShowProductRight/ShowProductRightComponents/TransportComponent/TransportComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="transport"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    transport_component: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style></style>
