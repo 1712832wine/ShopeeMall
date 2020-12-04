fbcc6a09dce5b6b558fd0e9a1048440d9f49afd9
NGUYỄN HỬU TRÍ <tri.nguyen@techupcorp.com>
2020-12-04 19:39:36 +0700

    [code] - discountcode:Done

---
+++ src/components/ProductContentDetails/ProductContentRight/DiscountCode/DiscountCode.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="discount-code">
+    <div class="title">{{ title }}</div>
+    <div>
+      <discount-item v-for="item in items" :key="item.id" :item="item" />
+    </div>
+  </div>
+</template>
+
+<script>
+import DiscountItem from "./DiscountItem.vue";
+import Data from "../../../../data/discountcodeData.json";
+export default {
+  data: function() {
+    return Data.discount_code;
+  },
+  components: {
+    "discount-item": DiscountItem,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./Style/Style__DiscountCode.scss";
+</style>
--- src/components/ProductContentDetails/ProductContentRight/ProductContentRight.vue
+++ src/components/ProductContentDetails/ProductContentRight/ProductContentRight.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="page-product__content--right">
+    <discount-code />
     <topselling />
   </div>
 </template>
 
 <script>
 import TopSellingProduct from "./TopSellingProduct/TopSellingProduct.vue";
-export default { components: { topselling: TopSellingProduct } };
+import DiscountCode from "./DiscountCode/DiscountCode.vue";
+export default {
+  components: { topselling: TopSellingProduct, "discount-code": DiscountCode },
+};
 </script>
 
 <style lang="scss" scoped>
